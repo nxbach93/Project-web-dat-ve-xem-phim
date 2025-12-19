eafdc01d16fcb39e3d4cfcd98d9aaf67d14d5f8d
Kieukien153 <kienkt153@gmail.com>
2025-12-19 22:28:04 +0700

    Dang nhap & Dang ky

---
+++ DangNhap/DangKy/login.php
@@ -0,0 +1,16 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Chọn vai trò</title>
+</head>
+<body>
+
+<h2>Chọn vai trò đăng nhập</h2>
+
+<a href="formDangNhapNV.php">Nhân viên</a> |
+<a href="formDangNhapKH.php">Khách hàng</a>
+
+</body>
+</html>
