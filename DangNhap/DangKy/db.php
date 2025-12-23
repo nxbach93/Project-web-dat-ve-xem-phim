b8e3ce08719b63e1bc90cb9ed41734f982fbd685
Xuan Bach <bachvijt@gmail.com>
2025-12-23 13:21:47 +0700

    tttk sualai

--- DangNhap/DangKy/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "cinema");
-if (!$conn) {
-    die("Lỗi kết nối CSDL: " . mysqli_connect_error());
-}
-?>
--- headfoot/connect.php
+++ headfoot/connect.php
@@ -5,3 +5,4 @@ if ($conn->connect_error) {
 }
 $conn->set_charset("utf8mb4");
 
+
